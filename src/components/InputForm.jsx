e65e41e5f878ee09fc8bafeeafbac87a38fba5a8
Narongsak <narongsak.jaro@gmail.com>
2025-05-29 17:41:57 +0700

    refactor(input-form): make component reusable using children prop

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,50 +1,5 @@
-const InputForm = ({
-  name,
-  price,
-  ref,
-  onEnterName,
-  onEnterPrice,
-  onEnterRef,
-  onInsertImage,
-  onAddItem,
-  onExportData,
-}) => {
-  return (
-    <form className="flex flex-col gap-5 p-6">
-      <input
-        className="block w-full"
-        type="text"
-        placeholder="NAME"
-        value={name}
-        onChange={onEnterName}
-        required
-      />
-      <input
-        className="block w-full"
-        type="text"
-        placeholder="PRICE"
-        value={price}
-        onChange={onEnterPrice}
-        required
-      />
-      <input
-        className="block w-full"
-        type="text"
-        placeholder="URL"
-        value={ref}
-        onChange={onEnterRef}
-      />
-      <input className="block w-full" type="file" onChange={onInsertImage} />
-      <div className="flex gap-3">
-        <button type="button" className="flex-auto" onClick={onAddItem}>
-          SUBMIT
-        </button>
-        <button type="button" className="flex-auto" onClick={onExportData}>
-          DELETE
-        </button>
-      </div>
-    </form>
-  );
+const InputForm = ({ children }) => {
+  return <form className="flex flex-col gap-5 p-6">{children}</form>;
 };
 
 export default InputForm;
